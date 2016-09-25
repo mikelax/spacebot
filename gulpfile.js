fb5f76259c4c8be91284f9c6f7c44a19066327e6
Michael Holtzman <mholtzman@gmail.com>
2016-09-24 22:58:55 -0400

    MISC Update travis and buildfile.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,6 @@ const gulp = require('gulp-help')(require('gulp'));
 const gutil = require('gulp-util');
 const mocha = require('gulp-mocha');
 
-function errorHandler(error) {
-  gutil.log(error.toString());
-  this.emit('end');
-}
-
 // Top level tasks
 gulp.task('test', 'Run Unit Tests', ['lint'], (cb) => gulp
   .src('test/*.spec.js', { read: false })
@@ -17,7 +12,7 @@ gulp.task('test', 'Run Unit Tests', ['lint'], (cb) => gulp
     ui: 'bdd',
     timeout: 1000
   }))
-  .on('error', errorHandler)
+  .on('error', gutil.log)
   .on('finish', cb)
 );
 
@@ -26,5 +21,5 @@ gulp.task('lint', 'ESLint all Javascript', () => gulp
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
-  .on('error', errorHandler)
+  .on('error', gutil.log)
 );
