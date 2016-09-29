72eea72c045a00eaee1285ab93c8d5c2ed9a0231
Michael Holtzman <mholtzman@gmail.com>
2016-09-29 14:26:12 -0400

    Store oauth token after successful login.

--- functions/oauth/handler.js
+++ functions/oauth/handler.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const dynamodb = require('../lib/dynamodb');
 const errorToJsonAndLog = require('../lib/errors').errorToJsonAndLog;
 const slack = require('../lib/slack');
 
 module.exports.oauth = function oauth(event, context, cb) {
   slack.exchangeCodeForToken(event)
   .then((resp) => {
-    console.log(resp);
+    dynamodb.saveOAuthToken(resp.user_id,
+      resp.team_id,
+      resp.access_token,
+      resp.scope,
+      resp.team_name);
+  })
+  .then(() => {
     cb(null, { location: process.env.OAUTH_SUCCESS_URL });
   })
   .catch((e) => {
